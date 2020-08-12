afd670e6ed6b0c79b277433739d8211b193e6547
Godm0de <Godm0de@example.com>
2020-08-12 15:14:01 +0200

    reorder

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
+var transition = ()=>{
+    document.documentElement.classList.add("transition");
+
+    window.setTimeout(()=>{
+        document.documentElement.classList.remove("transition");
+    }, 1000);
+}
+
+
+
 document.getElementsByClassName("theme-button")[0].addEventListener('change', ()=>{
     const checkbox = document.getElementsByClassName("theme-button")[0];
 
     if(checkbox.checked){
+        transition();
         document.documentElement.setAttribute('theme', 'dark');
     }else{
+        transition();
         document.documentElement.setAttribute('theme', 'light');
     }
 })
+
